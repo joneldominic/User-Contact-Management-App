04eedbe8960b04bac84be46a023b931fda1276ef
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-01-20 21:59:46 +0800

    Refactor Contact Context

--- frontend/src/context/contact-context.js
+++ frontend/src/context/contact-context.js
@@ -1,45 +1,20 @@
 import React, { useState, useEffect, useContext } from "react";
 import {
-  addNewContact,
   deleteContact,
   getContacts,
-  updateContact,
 } from "../service/contact-service";
 import AuthContext from "./auth-context";
-import { useHistory } from "react-router-dom";
 
 const ContactContext = React.createContext({
   contactList: [],
-  createContact: ({
-    firstname,
-    middlename,
-    lastname,
-    number,
-    email,
-    title,
-    address1,
-    address2,
-    notes,
-  }) => {},
-  updateContact: ({
-    id,
-    firstname,
-    middlename,
-    lastname,
-    number,
-    email,
-    title,
-    address1,
-    address2,
-    notes,
-  }) => {},
+  createContact: () => {},
+  updateContact: () => {},
   deleteContact: (contactId) => {},
   getContact: (contactId) => {},
 });
 
 export const ContactContextProvider = (props) => {
   const authCtx = useContext(AuthContext);
-  const history = useHistory();
 
   const [contactList, setContactList] = useState([]);
   const [listOutdated, setListOutdated] = useState(true);
@@ -68,22 +43,8 @@ export const ContactContextProvider = (props) => {
     setListOutdated(true);
   };
 
-  const onUpdateContactHandler = (contactInfo) => {
-    updateContact(authCtx.authUser.id, contactInfo)
-      .then((_) => {
-        console.log("From Contact Context On Update");
-        history.replace("/contacts");
-        setListOutdated(true);
-      })
-      .catch((err) => {
-        console.log(err.response);
-        alert(
-          err.response.data.apierror.subErrors.map(
-            (_error) =>
-              `field: ${_error.field}  |  message: ${_error.message}\n\n`
-          )
-        );
-      });
+  const onUpdateContactHandler = () => {
+    setListOutdated(true);
   };
 
   const onDeleteContact = (contactId) => {
