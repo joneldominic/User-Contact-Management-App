e4bdf628842f374ed0b572cb423154e7e77338d3
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2022-02-10 15:38:20 +0800

    Fix On Add Contact Bug

--- frontend/src/components/ContactList/ContactList.js
+++ frontend/src/components/ContactList/ContactList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import SearchBar from "./SearchBar";
 import FloatingAddButton from "./FloatingAddButton";
 import ContactListItem from "./ContactListItem";
+import NoContact from "./NoContact";
+import Modal from "../../core/UI/Modal";
 
 import {
   ContactListContainer,
@@ -16,19 +18,23 @@ import {
   LoadingSpinner,
 } from "./styles";
 
-import { getContacts } from "../../redux/contact-slice";
-import NoContact from "./NoContact";
-
+import { getContacts, contactActions } from "../../redux/contact-slice";
 import { uiActions } from "../../redux/ui-slice";
+
+import AppRoutes from "../../constants/app-routes";
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const location = useLocation();
 
   const { id: userId } = useSelector((state) => state.auth.user);
   const contactList = useSelector((state) => state.contact.contactList);
-  const hasPending = useSelector((state) => state.contact.hasPending);
   const isLoading = useSelector((state) => state.contact.isLoading);
+  const hasPending = useSelector((state) => state.contact.hasPending);
+  const { show: showModal, id: modalId } = useSelector(
+    (state) => state.ui.modal
+  );
 
   const [filteredContactList, setFilteredContactList] = useState([]);
 
@@ -55,40 +61,80 @@ const ContactList = () => {
   };
 
   const onAddButtonClickHandler = () => {
+    const { pathname } = location;
+
+    if (pathname === `${AppRoutes.ContactPage.path}/new`) {
+      return;
+    }
+
     if (hasPending.status) {
-      dispatch(uiActions.setModal({ show: true, id: hasPending.from }));
+      dispatch(uiActions.setModal({ show: true, id: "contactlist" }));
     } else {
-      history.push("/contacts/new");
+      history.push(`${AppRoutes.ContactPage.path}/new`);
     }
   };
 
+  const onModalConfirmClickHandler = () => {
+    dispatch(
+      contactActions.setPending({
+        status: false,
+        from: "",
+      })
+    );
+    dispatch(uiActions.setModal({ show: false, id: "" }));
+    history.push(`${AppRoutes.ContactPage.path}/new`);
+  };
+
+  const onModalCancelClickHandler = () => {
+    dispatch(uiActions.setModal({ show: false, id: "" }));
+  };
+
   return (
-    <ContactListContainer>
-      <ContactListCard>
-        <ContactListCardContent>
-          <SearchBar
-            onChange={onChangeKeywordHandler}
-            onClear={onClearFilterHandler}
-          />
-          <Divider />
-          <ContactListItemContainer>
-            {isLoading && <LoadingSpinner />}
-            {!isLoading &&
-              filteredContactList.length > 0 &&
-              filteredContactList.map((_contact) => (
-                <ContactListItem key={_contact.id} contact={_contact} />
-              ))}
-            {!isLoading && filteredContactList.length === 0 && (
-              <NoContact
-                message="No Contact Found"
-                subMessage="Try adding one using the button below."
-              />
-            )}
-          </ContactListItemContainer>
-        </ContactListCardContent>
-      </ContactListCard>
-      <FloatingAddButton onClick={onAddButtonClickHandler} />
-    </ContactListContainer>
+    <>
+      {showModal && modalId === "contactlist" && (
+        <Modal
+          color="warning"
+          title="Are you sure?"
+          message="Changes will not be save. This process cannot be undone."
+          option1={{
+            color: "primary",
+            label: "Confirm",
+            callback: onModalConfirmClickHandler,
+          }}
+          option2={{
+            color: "warning",
+            label: "Cancel",
+            callback: onModalCancelClickHandler,
+          }}
+        />
+      )}
+      <ContactListContainer>
+        <ContactListCard>
+          <ContactListCardContent>
+            <SearchBar
+              onChange={onChangeKeywordHandler}
+              onClear={onClearFilterHandler}
+            />
+            <Divider />
+            <ContactListItemContainer>
+              {isLoading && <LoadingSpinner />}
+              {!isLoading &&
+                filteredContactList.length > 0 &&
+                filteredContactList.map((_contact) => (
+                  <ContactListItem key={_contact.id} contact={_contact} />
+                ))}
+              {!isLoading && filteredContactList.length === 0 && (
+                <NoContact
+                  message="No Contact Found"
+                  subMessage="Try adding one using the button below."
+                />
+              )}
+            </ContactListItemContainer>
+          </ContactListCardContent>
+        </ContactListCard>
+        <FloatingAddButton onClick={onAddButtonClickHandler} />
+      </ContactListContainer>
+    </>
   );
 };
 
